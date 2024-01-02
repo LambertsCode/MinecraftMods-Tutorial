ee29365c8b380d71e33b07427ae5576099300002
LeahMiller <lambertscode@gmail.com>
2024-01-02 01:33:28 -0700

    End of Tutorial 8: added tomato

--- src/main/java/net/lambert/tutorialmod/item/ModItems.java
+++ src/main/java/net/lambert/tutorialmod/item/ModItems.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lambert.tutorialmod.TutorialMod;
 import net.lambert.tutorialmod.item.custom.MetalDetectorItem;
+import net.lambert.tutorialmod.item.custom.ModFoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registerable;
@@ -19,6 +20,8 @@ public class ModItems {
 
     public static final Item METAL_DETECTOR = registerItem("metal_detector",
             new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
+    public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings().food(ModFoodComponents.TOMATO)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
